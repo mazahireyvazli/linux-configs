7771791878a58817c1db0e9169e7b5367913cd5f
mazahireyvazli <mazahir.eyvazli@gmail.com>
2024-02-14 20:58:46 +0100

    setup remote url based configs

--- setup-git.sh
+++ setup-git.sh
@@ -6,15 +6,16 @@
 read -r -p "Install git cli? [y/N] " should_install_git
 if [[ "$should_install_git" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
+    # ubuntu specific
+    sudo apt-add-repository ppa:git-core/ppa
+    # end of ubuntu specific
     sudo ${pkg_manager} update -y
     sudo ${pkg_manager} install git
 else
     echo "skipping git installation"
 fi
 
-cp -r ./gitconfig/.gitconfig ${HOME}
+cp -r ./gitconfig/. ${HOME}
 
 [ -f ~/.zshrc ] && echo -e '\nexport GPG_TTY=$(tty)' >> ~/.zshrc  
 [ -f ~/.bashrc ] && echo -e '\nexport GPG_TTY=$(tty)' >> ~/.bashrc
-
-echo "copy git configs per user manually and update access tokens and signing keys"
